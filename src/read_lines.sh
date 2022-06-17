00fb6f39f0496bfb0534d799bb60214747bdc97f
Shiyi Shen <shiys3447@yahoo.com>
2022-06-17 14:40:24 -0400

    Update read_lines.sh

--- src/read_lines.sh
+++ src/read_lines.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 
-
 while read p; do
-  echo "$p"
-done <outfile.csv
+  delimiter=","
+  array=();
+  IFS=',' read -r -a array <<< "$p"
+  echo ${array[1]}
+  facebook-scraper -c src/cookies.json -p 100 --filename ${array[1]} ${array[3]}
+done <data/outfile.csv
