382aca5934b22f976fee2b44e680886e80a34442
Shiyi Shen <shiys3447@yahoo.com>
2022-06-23 19:01:37 -0400

    Update read_lines.sh

--- src/read_lines.sh
+++ src/read_lines.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-
-while read p; do
-  delimiter=","
-  array=();
-  IFS=',' read -r -a array <<< "$p"
-  echo ${array[1]}
-  facebook-scraper -c src/cookies.json -p 100 --filename data/${array[1]} ${array[3]}
-done <data/outfile.csv
+git clone https://github.com/tweepy/tweepy.git
+cd tweepy
+pip install .
+pip install pandas
+python script/pac_tweet.py
+# while read p; do
+#   delimiter=","
+#   array=();
+#   IFS=',' read -r -a array <<< "$p"
+#   echo ${array[1]}
+#   facebook-scraper -c src/cookies.json -p 100 --filename data/${array[1]} ${array[3]}
+# done <data/outfile.csv
