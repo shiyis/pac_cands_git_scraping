9484e2fe675705d37c6c928000558614f5c13540
Shiyi Shen <shiys3447@yahoo.com>
2022-06-25 15:07:26 -0400

    Update read_lines.sh

--- src/read_lines.sh
+++ src/read_lines.sh
@@ -7,16 +7,16 @@ pip install .
 pip install pandas
 cd ../
 pwd
+python ./script/pac_tweet.py
 
 
-
-while read p; do
-  delimiter=","
-  array=();
-  IFS=',' read -r -a array <<< "$p"
-  echo ${array[0]}
-  echo ${array[3]}
-  python ./script/pac_tweet.py --file ./data/${array[0]} --name ${array[3]}
-  sleep 5
-done <./data/outfile.csv 
+# while read p; do
+#   delimiter=","
+#   array=();
+#   IFS=',' read -r -a array <<< "$p"
+#   echo ${array[0]}
+#   echo ${array[3]}
+#   python ./script/pac_tweet.py --file ./data/${array[0]} --name ${array[3]}
+#   sleep 5
+# done <./data/outfile.csv 
 
